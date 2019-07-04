56c86c3360b2419b58747a6db6ecca7715c6e38f
Stadicus <stadicus@protonmail.com>
2019-07-04 14:27:33 +0200

    improve/harden startup-checks

--- armbian/base/scripts/systemd-startup-checks.sh
+++ armbian/base/scripts/systemd-startup-checks.sh
@@ -19,44 +19,38 @@ ifmetric eth0 10
 timedatectl set-ntp true
 echo "180" > /sys/class/hwmon/hwmon0/pwm1
 
+# UART configuration
+# ------------------------------------------------------------------------------
+# TODO(Stadicus): Adjust to new Mender configuration
+
 # disable serial output on UART0
-if ! grep -Fiq "console=display" /boot/armbianEnv.txt; then
-  if ! grep -Fiq "console=" /boot/armbianEnv.txt; then
-    echo "console=display" >> /boot/armbianEnv.txt
-  else
-    sed -i '/console=/Ic\console=display' /boot/armbianEnv.txt
-  fi
-  mkimage -C none -A arm -T script -d /boot/boot.cmd /boot/boot.scr > /opt/shift/config/uartconfig.log
+# if ! grep -Fiq "console=display" /boot/armbianEnv.txt; then
+#   if ! grep -Fiq "console=" /boot/armbianEnv.txt; then
+#     echo "console=display" >> /boot/armbianEnv.txt
+#   else
+#     sed -i '/console=/Ic\console=display' /boot/armbianEnv.txt
+#   fi
+#   mkimage -C none -A arm -T script -d /boot/boot.cmd /boot/boot.scr > /opt/shift/config/uartconfig.log
+#
+#   UART_REBOOT=0
+#   [ -f "${SYSCONFIG_PATH}/UART_REBOOT" ] && source "${SYSCONFIG_PATH}/UART_REBOOT"
+#   if [ ${UART_REBOOT} -eq 1 ]; then 
+#     echo "ERR: previous UART_REBOOT not successful, check system"
+#   else
+#     echo "UART_REBOOT=1" > /opt/shift/sysconfig/UART_REBOOT
+#     reboot
+#   fi
 
-  source /opt/shift/sysconfig/UART_REBOOT
-  if [ ${UART_REBOOT} -eq 1 ]; then 
-    echo "ERR: previous UART_REBOOT not successful, check system"
-  else
-    echo "UART_REBOOT=1" > /opt/shift/sysconfig/UART_REBOOT
-    reboot
-  fi
+# else
+#   echo "UART_REBOOT=0" > /opt/shift/sysconfig/UART_REBOOT
+# fi
 
-else
-  echo "UART_REBOOT=0" > /opt/shift/sysconfig/UART_REBOOT
-fi
+# SSD configuration
+# ------------------------------------------------------------------------------
+## check if SSD mount is configured in /etc/fstab (trailing space is essential!)
+if ! grep -q '/mnt/ssd ' /etc/fstab ; then
 
-# check if swapfile exists on ssd
-if [ ! -f /mnt/ssd/swapfile ]; then
-  fallocate --length 2GiB /mnt/ssd/swapfile
-  chmod 600 /mnt/ssd/swapfile
-  mkswap /mnt/ssd/swapfile
-  swapon /mnt/ssd/swapfile
-fi
-
-# check if swapfile is configured in /etc/fstab, and add it if necessary
-if ! grep -q '/mnt/ssd/swapfile' /etc/fstab ; then
-  echo "/mnt/ssd/swapfile swap swap defaults 0 0" >> /etc/fstab
-fi
-
-# check if SSD mount is configured in /etc/fstab
-if ! grep -q '/mnt/ssd' /etc/fstab ; then
-
-  # valid partition present?
+  ## valid partition present?
   if lsblk | grep -q 'nvme0n1p1'; then
     echo "/dev/nvme0n1p1 /mnt/ssd ext4 rw,nosuid,dev,noexec,noatime,nodiratime,auto,nouser,async,nofail 0 2" >> /etc/fstab
 
@@ -64,7 +58,9 @@ if ! grep -q '/mnt/ssd' /etc/fstab ; then
     echo "/dev/sda1 /mnt/ssd ext4 rw,nosuid,dev,noexec,noatime,nodiratime,auto,nouser,async,nofail 0 2" >> /etc/fstab
 
   else
-    # if no valid partition present, is image configured for autosetup of SSD?
+    ## if no valid partition present, is image configured for autosetup of SSD?
+    
+    AUTOSETUP_SSD=0
     [ -f "${SYSCONFIG_PATH}/AUTOSETUP_SSD" ] && source "${SYSCONFIG_PATH}/AUTOSETUP_SSD"
 
     if ! mountpoint /mnt/ssd -q && [[ ${AUTOSETUP_SSD} -eq 1 ]]; then
@@ -74,7 +70,7 @@ if ! grep -q '/mnt/ssd' /etc/fstab ; then
       fi
     fi
 
-    # check for newly created partition
+    ## check for newly created partition
     if lsblk | grep -q 'nvme0n1p1'; then
       echo "/dev/nvme0n1p1 /mnt/ssd ext4 rw,nosuid,dev,noexec,noatime,nodiratime,auto,nouser,async,nofail 0 2" >> /etc/fstab
     elif lsblk | grep -q 'sda1'; then
@@ -91,40 +87,55 @@ fi
 
 sudo mount -a
 
-# abort check if SSD mount is not successful
+## abort check if SSD mount is not successful
 if ! mountpoint /mnt/ssd -q; then 
   echo "Mounting of SSD failed"
   exit 1
 fi
 
-# mount potentially updated /etc/fstab or new swap file
+# Swap configuration
+# ------------------------------------------------------------------------------
+## check if swapfile exists on ssd
+if [ ! -f /mnt/ssd/swapfile ]; then
+  fallocate --length 2GiB /mnt/ssd/swapfile
+  chmod 600 /mnt/ssd/swapfile
+  mkswap /mnt/ssd/swapfile
+  swapon /mnt/ssd/swapfile
+fi
+
+## check if swapfile is configured in /etc/fstab, and add it if necessary
+if ! grep -q '/mnt/ssd/swapfile' /etc/fstab ; then
+  echo "/mnt/ssd/swapfile swap swap defaults 0 0" >> /etc/fstab
+fi
+
+## mount potentially updated /etc/fstab
 mount -a
 
-# create missing directories & always set correct owner
-# access control lists (setfacl) are used to control permissions of newly created files 
-chown bitcoin:system /mnt/ssd 
+# Folders & permissions
+# ------------------------------------------------------------------------------
+## create missing directories & always set correct owner
+## access control lists (setfacl) are used to control permissions of newly created files 
+chown bitcoin:system /mnt/ssd/bitcoin 
 
-# bitcoin data storage
+## bitcoin data storage
 mkdir -p /mnt/ssd/bitcoin/.bitcoin/testnet3
 chown -R bitcoin:bitcoin /mnt/ssd/bitcoin/
+chmod -R 750 /mnt/ssd/bitcoin/
 setfacl -dR -m g::rx /mnt/ssd/bitcoin/.bitcoin/
 setfacl -dR -m o::- /mnt/ssd/bitcoin/.bitcoin/
 
-# electrs data storage
+## electrs data storage
 mkdir -p /mnt/ssd/electrs/
 chown -R electrs:bitcoin /mnt/ssd/electrs/
+chmod -R 750 /mnt/ssd/electrs/
 
-# system folders
+## system folders
 mkdir -p /mnt/ssd/prometheus
 chown -R prometheus:system /mnt/ssd/prometheus/
 mkdir -p /mnt/ssd/system/journal/
 
-# set permissions for whole ssd 
-# (user:rwx group:r-x other:---)
-chmod -R 750 /mnt/ssd
-
-# We set rpccookiefile=/mnt/ssd/bitcoin/.bitcoin/.cookie, but there seems to be
-# no way to specify where to expect the bitcoin cookie for c-lightning, so let's
-# create a symlink at the expected testnet location.
+## We set rpccookiefile=/mnt/ssd/bitcoin/.bitcoin/.cookie, but there seems to be
+## no way to specify where to expect the bitcoin cookie for c-lightning, so let's
+## create a symlink at the expected testnet location.
 mkdir -p /mnt/ssd/bitcoin/.bitcoin/testnet3/
 ln -fs /mnt/ssd/bitcoin/.bitcoin/.cookie /mnt/ssd/bitcoin/.bitcoin/testnet3/.cookie
