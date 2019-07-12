87bed385f7d8decf9ab92f2472b0285dc2057bf5
Stadicus <stadicus@protonmail.com>
2019-07-12 08:51:50 +0200

    update Dist & kernel in script

--- scripts/move-armbian-image.sh
+++ scripts/move-armbian-image.sh
@@ -7,9 +7,10 @@ set -eu
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # TODO(hkjn): Consider defining these config values somewhere higher-level than inside this script.
-KERNEL_VERSION="${KERNEL_VERSION:-4.4.182}"
+KERNEL_VERSION="${KERNEL_VERSION:-4.4.184}"
 ARMBIAN_VERSION="${ARMBIAN_VERSION:-5.91}"
-IMG_FILE="${SCRIPT_DIR}/../armbian/armbian-build/output/images/Armbian_${ARMBIAN_VERSION}_Rockpro64_Debian_buster_default_${KERNEL_VERSION}.img"
+DISTRIBUTION="Ubuntu_bionic"
+IMG_FILE="${SCRIPT_DIR}/../armbian/armbian-build/output/images/Armbian_${ARMBIAN_VERSION}_Rockpro64_${DISTRIBUTION}_default_${KERNEL_VERSION}.img"
 TARGET_FILE="BitBoxBase_Armbian_RockPro64.img"
 
 if [[ ! -e "${IMG_FILE}" ]]; then
