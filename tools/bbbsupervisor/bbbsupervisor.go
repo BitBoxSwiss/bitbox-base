1b4c313e39d0a2f8f39234433489c0bb92359a62
0xb10c <0xb10c@gmail.com>
2019-07-24 11:42:17 +0200

    Refactor prometheusWatcher.query() and add ElectrsNoBitcoindConnectivty handler. (see digitalbitbox/bitbox-base/issues/115). Script or command to recrate bitcoind .cookie is still a TODO.
    Replaces the somewhat error prone json decoding. Now using gjson to get
    values from the returned JSON. Introduces the dependency on gjson but
    makes the query() method more flexibile and expandable for the future.
    
    https://github.com/tidwall/gjson

--- tools/bbbsupervisor/bbbsupervisor.go
+++ tools/bbbsupervisor/bbbsupervisor.go
@@ -45,22 +45,23 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"os/exec"
-	"strconv"
 	"strings"
 	"time"
+
+	"github.com/tidwall/gjson"
 )
 
 type watcher interface {
 	watch()
 }
 
-// serviceEvent represents an actionable event from a systemd service that we are following
+// serviceEvent represents an actionable event from a systemd service that we are watching
 // e.g. that bitcoin or electrs has fully synced, or a service os not reachable
 type serviceEvent struct {
 	unit    string  // unit represents systemd unit name, e.g. 'bitcoind'
@@ -71,7 +72,7 @@ type serviceEvent struct {
 
 // logWatcher watches systemd service logs.
 type logWatcher struct {
-	unit   string            // systemd unit to follow, e.g 'bitcoind'
+	unit   string            // systemd unit to watch, e.g 'bitcoind'
 	events chan serviceEvent // channel for passing service Events (e.g. a systemd log entry)
 	errs   chan error        // channel for passing errors (e.g. stderr outputs)
 }
@@ -87,7 +88,7 @@ type prometheusWatcher struct {
 	errs       chan error        // channel for passing errors (e.g. stderr outputs)
 }
 
-// watchers represents several follower objects.
+// watchers represents several watcher objects.
 type watchers []watcher
 
 // errWriter implements io.Writer and writes all contents as error into the wrapped chan.
@@ -108,16 +109,18 @@ const versionNum = 0.1
 const prometheusURL = "http://localhost:9090"
 
 const (
-	triggerFullySynced = 1 + iota
+	triggerElectrsFullySynced = 1 + iota
+	triggerElectrsNoBitcoindConnectivity
 	triggerUnitDown
-	triggerMeasureUpdate
+	triggerPrometheusMeasureUpdate
 )
 
 // Map of possible triggers. Mapped by their trigger to a trigger name
 var triggerNames = map[trigger]string{
-	triggerFullySynced:   "fullySynced",
-	triggerUnitDown:      "unitDown",
-	triggerMeasureUpdate: "measureUpdate",
+	triggerElectrsFullySynced:            "electrsFullySynced",
+	triggerElectrsNoBitcoindConnectivity: "electrsNoBitcoindConnectivity",
+	triggerUnitDown:                      "unitDown",
+	triggerPrometheusMeasureUpdate:       "prometheusMeasureUpdate",
 }
 
 // String returns a human readable value for a trigger
@@ -187,63 +190,62 @@ func (pw prometheusWatcher) watch() {
 }
 
 // query queries prometheus with the specified expression
+// query _currently_ only supports quering for integer values as used in the bitcoind_idb query
 func (pw prometheusWatcher) query() {
 
 	httpResp, err := http.Get(pw.server + "/api/v1/query?query=" + pw.expression)
 	if err != nil {
-		pw.errs <- fmt.Errorf("failed to fetch %q from prometheus server: %v", pw.expression, err)
+		pw.errs <- fmt.Errorf("Failed to fetch %q from prometheus server: %v", pw.expression, err)
 		return
 	}
 	defer httpResp.Body.Close()
 
-	type Response struct {
-		Status string `json:"status"`
-		Data   struct {
-			ResultType string `json:"resultType"`
-			Result     []struct {
-				Value []interface{} `json:"value"`
-			} `json:"result"`
-		} `data:"data"`
-	}
-
-	var resp Response
-	if err := json.NewDecoder(httpResp.Body).Decode(&resp); err != nil {
-		pw.errs <- fmt.Errorf("failed to read response body from prometheus request for %q: %v", pw.expression, err)
+	body, err := ioutil.ReadAll(httpResp.Body)
+	if err != nil {
+		pw.errs <- fmt.Errorf("Failed to read response body from prometheus request for %q: %v", pw.expression, err)
 		return
 	}
 
-	//fmt.Printf("xx: decoded json: %+v\n", resp)
-	if resp.Status != "success" {
-		pw.errs <- fmt.Errorf("prometheus request for %q returned non-success: %v", pw.expression, resp)
+	// Check for a valid response by checking:
+	// - checking if the response is valid json
+	// - the `status` == success
+	// - the list `data.result` having one and only one entry
+	// - the value list `data.result[0].value` having exactly two entires
+	// - there exists a response value for our expression `data.result[0].value[1]`
+
+	bodyAsString := string(body)
+
+	if !gjson.Valid(bodyAsString) {
+		pw.errs <- fmt.Errorf("Prometheus request for %q returned invalid JSON: %v", pw.expression, bodyAsString)
 		return
 	}
 
-	if len(resp.Data.Result) > 1 {
-		pw.errs <- fmt.Errorf("unexpectedly got %d results from prometheus request for %q: %+v", len(resp.Data.Result), pw.expression, resp)
+	status := gjson.Get(bodyAsString, "status").String()
+	if status != "success" {
+		pw.errs <- fmt.Errorf("Prometheus request for %q returned non-success (%s): %v", pw.expression, status, bodyAsString)
+		return
 	}
 
-	firstResult := resp.Data.Result[0]
-	if len(firstResult.Value) != 2 { // note: timestamp and value
-		pw.errs <- fmt.Errorf("unexpectedly got %d values from prometheus request for %q: %+v", len(firstResult.Value), pw.expression, resp)
+	queryResult := gjson.Get(bodyAsString, "data.result").Array()
+	if len(queryResult) != 1 {
+		pw.errs <- fmt.Errorf("Unexpectedly got %d results from prometheus request for %s: %s", len(queryResult), pw.expression, bodyAsString)
+		return
 	}
 
-	value := firstResult.Value[1]
-
-	//timestamp := firstResult.Value[0]
-	//fmt.Printf("xx: parsed value %+v (%T) at time %v (%T)\n", value, value, timestamp, timestamp)
-	switch v := value.(type) {
-	case string:
-		val64, err := strconv.ParseFloat(value.(string), 64)
-
-		if err != nil {
-			fmt.Printf("could not convert value %v of type %T into float64", value, value)
-		}
-
-		//fmt.Printf("returned %v, %v, %v, %v \n", pw.unit, triggerMeasureUpdate, pw.measure, val64)
-		pw.events <- serviceEvent{unit: pw.unit, trigger: triggerMeasureUpdate, measure: pw.measure, value: val64}
-	default:
-		pw.errs <- fmt.Errorf("unknown type of value %v (%T)", v, v)
+	firstResultValue := queryResult[0].Map()["value"].Array()
+	if len(firstResultValue) != 2 {
+		pw.errs <- fmt.Errorf("Unexpectedly got %d values from prometheus request for %d: %s", len(firstResultValue), pw.expression, bodyAsString)
+		return
 	}
+
+	if firstResultValue[1].Exists() == false {
+		pw.errs <- fmt.Errorf("The result value for %s does not exist: %s", pw.expression, bodyAsString)
+		return
+	}
+
+	measuredValue := firstResultValue[1].Float()
+	pw.events <- serviceEvent{unit: pw.unit, trigger: triggerPrometheusMeasureUpdate, measure: pw.measure, value: measuredValue}
+
 }
 
 // handleFlags parses command line arguments and handles them
@@ -283,9 +285,13 @@ func startWatchers(ws watchers) {
 func parseEvent(p []byte, unit string) *serviceEvent {
 	switch {
 	case strings.Contains(string(p), "finished full compaction"): // fully synched electrs
-		fmt.Printf("parseEvent: 'finished full compaction' %q", p)
-		return &serviceEvent{unit: unit, trigger: triggerFullySynced}
+		return &serviceEvent{unit: unit, trigger: triggerElectrsFullySynced}
+
+	case strings.Contains(string(p), "WARN - reconnecting to bitcoind: no reply from daemon"): // electrs unable to connect bitcoind
+		return &serviceEvent{unit: unit, trigger: triggerElectrsNoBitcoindConnectivity}
+
 	}
+
 	return nil
 }
 
@@ -293,14 +299,15 @@ func processEvents(events chan serviceEvent, errs chan error) {
 	for {
 		select {
 		case err := <-errs:
-			panic(fmt.Sprintf("fatal: error from watcher: %v\n", err))
+			panic(fmt.Sprintf("Fatal: Error from watcher: %v\n", err))
 		case event := <-events:
-			fmt.Printf("watcher passed on output: %v\n", event)
-
 			switch {
-			case event.trigger == triggerFullySynced:
+			case event.trigger == triggerElectrsFullySynced:
 				handleElectrsFullySynced(event)
-			case event.trigger == triggerMeasureUpdate:
+			case event.trigger == triggerElectrsNoBitcoindConnectivity:
+				handleElectrsNoBitcoindConnectivity(event)
+			case event.trigger == triggerPrometheusMeasureUpdate:
+
 				fmt.Printf("Unit %v update: %v is now %v\n", event.unit, event.measure, event.value)
 			}
 		}
@@ -330,6 +337,15 @@ func restartUnit(unit string) error {
 		fmt.Printf("restartUnit: command '%v' executed.'", cmdAsString)
 	}
 	return err
+}
+
+func handleElectrsNoBitcoindConnectivity(event serviceEvent) {
+	fmt.Printf("PLACEHOLDER: handling %s event should happen now.\n", event.trigger.String())
+
+	/* TODO:
+	recreate bitcoind cookie env file /mnt/ssd/bitcoin/.bitcoin/.cookie.env
+	restartUnit("electrs")
+	*/
 }
 
 func main() {
