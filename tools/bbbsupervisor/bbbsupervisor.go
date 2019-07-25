061eede04ed0ea7945eb6a98e0825dc8f7ecc995
0xb10c <0xb10c@gmail.com>
2019-07-25 15:02:34 +0200

    Adds supervisorState for handleBitcoindIDB() and a flood control with a minDelay beetween triggers Additionally cleanups and smaller variable and function name changes

--- tools/bbbsupervisor/bbbsupervisor.go
+++ tools/bbbsupervisor/bbbsupervisor.go
@@ -61,30 +61,30 @@ type watcher interface {
 	watch()
 }
 
-// serviceEvent represents an actionable event from a systemd service that we are watching
-// e.g. that bitcoin or electrs has fully synced, or a service os not reachable
-type serviceEvent struct {
+// watcherEvent represents an event triggered by a watcher
+// e.g. that bitcoin or electrs has fully synced, or a service is not reachable
+type watcherEvent struct {
 	unit    string  // unit represents systemd unit name, e.g. 'bitcoind'
-	trigger trigger // event could be 'fully synced', 'unit down' or 'measureUpdate'
-	measure string
-	value   float64
+	trigger trigger // trigger could be e.g. 'triggerElectrsNoBitcoindConnectivity' or 'triggerPrometheusBitcoindIDB'
+	measure string  // measure is something that is measured by the prometheusWatcher
+	value   float64 // value is the value that has been measured
 }
 
 // logWatcher watches systemd service logs.
 type logWatcher struct {
 	unit   string            // systemd unit to watch, e.g 'bitcoind'
-	events chan serviceEvent // channel for passing service Events (e.g. a systemd log entry)
+	events chan watcherEvent // channel for passing service Events (e.g. a systemd log entry)
 	errs   chan error        // channel for passing errors (e.g. stderr outputs)
 }
 
-// prometheusWatcher watches metrics exposed by a Prometheus server.
+// prometheusWatcher watches metrics exposed by a Prometheus server
 type prometheusWatcher struct {
-	unit       string            // unit is the systemd unit that the measure belongs to (e.g. 'bitcoind')
-	measure    string            // measure is the name of the datapoint
-	expression string            // expression is the PQL expression to query for. If empty, measure is used.
+	unit       string            // unit is the systemd unit that the expression belongs to (e.g. 'bitcoind')
+	expression string            // expression is the PQL expression to query for.
 	server     string            // server is the address of the prometheus server to query from
+	trigger    trigger           // trigger is the trigger to fire when a expression has been read by this watcher
 	interval   time.Duration     // interval query interval
-	events     chan serviceEvent // channel for passing service Events (e.g. a systemd log entry)
+	events     chan watcherEvent // channel for passing service Events (e.g. a systemd log entry)
 	errs       chan error        // channel for passing errors (e.g. stderr outputs)
 }
 
@@ -94,12 +94,16 @@ type watchers []watcher
 // errWriter implements io.Writer and writes all contents as error into the wrapped chan.
 type errWriter struct{ errs chan error }
 
-// logWriter implements io.Writer and writes all contents as string into the wrapped chan.
-type logWriter struct{ logs chan string }
-
 type eventWriter struct {
-	events chan serviceEvent
+	events chan watcherEvent
 	unit   string
+}
+
+// supervisorState implements a current state for the supervisor.
+// the state values are filled over time
+type supervisorState struct {
+	triggerLastExecuted    map[trigger]int64 // implements a state (timestamps) when a trigger was fired (to mitigate trigger flooding)
+	prometheusLastStateIBD float64           // implements a state for the last `bitcoind_ibd` measurement value (to detect switches idb <-> no-idb)
 }
 
 // trigger is something specific that can happen for a service
@@ -111,16 +115,14 @@ const prometheusURL = "http://localhost:9090"
 const (
 	triggerElectrsFullySynced = 1 + iota
 	triggerElectrsNoBitcoindConnectivity
-	triggerUnitDown
-	triggerPrometheusMeasureUpdate
+	triggerPrometheusBitcoindIDB
 )
 
 // Map of possible triggers. Mapped by their trigger to a trigger name
 var triggerNames = map[trigger]string{
 	triggerElectrsFullySynced:            "electrsFullySynced",
 	triggerElectrsNoBitcoindConnectivity: "electrsNoBitcoindConnectivity",
-	triggerUnitDown:                      "unitDown",
-	triggerPrometheusMeasureUpdate:       "prometheusMeasureUpdate",
+	triggerPrometheusBitcoindIDB:         "prometheusBitcoindIDB",
 }
 
 // String returns a human readable value for a trigger
@@ -133,7 +135,7 @@ func (t trigger) String() string {
 
 // Write implements the io.Writer interface by sending the content as a parsed event through the event channel.
 func (ew eventWriter) Write(p []byte) (int, error) {
-	fmt.Printf("chanServiceEventWriter: %q\n", p) //TODO: this is debug output an can be removed in the future.
+	fmt.Printf("logWatcher event: %q\n", p) //TODO: this is debug output an can be removed in the future.
 	event := parseEvent(p, ew.unit)
 	if event != nil {
 		ew.events <- *event
@@ -169,7 +171,7 @@ func (lw logWatcher) watch() {
 	cmd.Stdout = eveWriter // stdout of journalctl is written into the events channel
 	cmd.Stderr = errWriter // stderr of journalctl is written into the errs channel
 
-	fmt.Printf("Watching journalctl for unit %s (%s) \n", lw.unit, cmdAsString)
+	fmt.Printf("Watching journalctl for unit %s (%s)\n", lw.unit, cmdAsString)
 
 	if err := cmd.Run(); err != nil {
 		errWriter.Write([]byte(fmt.Sprintf("failed to start cmd: %v", err)))
@@ -177,75 +179,84 @@ func (lw logWatcher) watch() {
 	errWriter.Write([]byte(fmt.Sprintf("command %v unexpectedly exited", cmdAsString)))
 }
 
-// watch implements watch interface by quering and watching values from a Prometheus server forever.
+// watch implements watch interface by querying and watching values from a Prometheus server forever.
 func (pw prometheusWatcher) watch() {
-	if len(pw.expression) == 0 {
-		pw.expression = pw.measure
-	}
 	for {
-		fmt.Printf("Querying prometheus for %q\n", pw.expression)
-		pw.query()
-		time.Sleep(pw.interval)
+		json, err := pw.queryJSON()
+		if err != nil {
+			pw.errs <- err
+			return
+		}
+
+		measuredValue, err := pw.parsePrometheusResponseAsFloat(json)
+		if err != nil {
+			pw.errs <- err
+			return
+		}
+
+		pw.events <- watcherEvent{unit: pw.unit, trigger: pw.trigger, measure: pw.expression, value: measuredValue}
+		time.Sleep(pw.interval) // TODO: replace sleep with wait group or context
 	}
 }
 
-// query queries prometheus with the specified expression
-// query _currently_ only supports quering for integer values as used in the bitcoind_idb query
-func (pw prometheusWatcher) query() {
+// query queries prometheus with the specified expression and returns the JSON as a string
+func (pw prometheusWatcher) queryJSON() (string, error) {
 
-	httpResp, err := http.Get(pw.server + "/api/v1/query?query=" + pw.expression)
+	client := http.Client{
+		Timeout: 5 * time.Second,
+	}
+
+	httpResp, err := client.Get(pw.server + "/api/v1/query?query=" + pw.expression)
 	if err != nil {
-		pw.errs <- fmt.Errorf("Failed to fetch %q from prometheus server: %v", pw.expression, err)
-		return
+		return "", fmt.Errorf("Failed to fetch %q from prometheus server: %v", pw.expression, err)
 	}
 	defer httpResp.Body.Close()
 
 	body, err := ioutil.ReadAll(httpResp.Body)
 	if err != nil {
-		pw.errs <- fmt.Errorf("Failed to read response body from prometheus request for %q: %v", pw.expression, err)
-		return
+		return "", fmt.Errorf("Failed to read response body from prometheus request for %q: %v", pw.expression, err)
 	}
 
-	// Check for a valid response by checking:
-	// - checking if the response is valid json
+	bodyAsString := string(body)
+
+	// check if the response is valid json
+	if !gjson.Valid(bodyAsString) {
+		return "", fmt.Errorf("Prometheus request for %q returned invalid JSON: %v", pw.expression, bodyAsString)
+	}
+
+	return bodyAsString, nil
+}
+
+// parsePrometheusResponseAsFloat parses a promethues JSON response and returns a float
+func (pw prometheusWatcher) parsePrometheusResponseAsFloat(json string) (float64, error) {
+
+	// Check for a valid prometheus json response by checking:
 	// - the `status` == success
 	// - the list `data.result` having one and only one entry
 	// - the value list `data.result[0].value` having exactly two entires
 	// - there exists a response value for our expression `data.result[0].value[1]`
 
-	bodyAsString := string(body)
-
-	if !gjson.Valid(bodyAsString) {
-		pw.errs <- fmt.Errorf("Prometheus request for %q returned invalid JSON: %v", pw.expression, bodyAsString)
-		return
-	}
-
-	status := gjson.Get(bodyAsString, "status").String()
+	status := gjson.Get(json, "status").String()
 	if status != "success" {
-		pw.errs <- fmt.Errorf("Prometheus request for %q returned non-success (%s): %v", pw.expression, status, bodyAsString)
-		return
+		return -1, fmt.Errorf("Prometheus request for %q returned non-success (%s): %v", pw.expression, status, json)
 	}
 
-	queryResult := gjson.Get(bodyAsString, "data.result").Array()
+	queryResult := gjson.Get(json, "data.result").Array()
 	if len(queryResult) != 1 {
-		pw.errs <- fmt.Errorf("Unexpectedly got %d results from prometheus request for %s: %s", len(queryResult), pw.expression, bodyAsString)
-		return
+		return -1, fmt.Errorf("Unexpectedly got %d results from prometheus request for %s: %s", len(queryResult), pw.expression, json)
 	}
 
 	firstResultValue := queryResult[0].Map()["value"].Array()
 	if len(firstResultValue) != 2 {
-		pw.errs <- fmt.Errorf("Unexpectedly got %d values from prometheus request for %d: %s", len(firstResultValue), pw.expression, bodyAsString)
-		return
+		return -1, fmt.Errorf("Unexpectedly got %d values from prometheus request for %d: %s", len(firstResultValue), pw.expression, json)
 	}
 
 	if firstResultValue[1].Exists() == false {
-		pw.errs <- fmt.Errorf("The result value for %s does not exist: %s", pw.expression, bodyAsString)
-		return
+		return -1, fmt.Errorf("The result value for %s does not exist: %s", pw.expression, json)
 	}
 
 	measuredValue := firstResultValue[1].Float()
-	pw.events <- serviceEvent{unit: pw.unit, trigger: triggerPrometheusMeasureUpdate, measure: pw.measure, value: measuredValue}
-
+	return measuredValue, nil
 }
 
 // handleFlags parses command line arguments and handles them
@@ -264,12 +275,12 @@ func printVersion() {
 }
 
 // setupWatchers sets up prometheusWatchers and logWatchers and returns them
-func setupWatchers(events chan serviceEvent, errs chan error) (ws watchers) {
+func setupWatchers(events chan watcherEvent, errs chan error) (ws watchers) {
 	return watchers{
 		logWatcher{"bitcoind", events, errs},
 		logWatcher{"lightningd", events, errs},
 		logWatcher{"electrs", events, errs},
-		prometheusWatcher{unit: "bitcoind", measure: "bitcoind_ibd", server: prometheusURL, interval: 10 * time.Second, events: events, errs: errs},
+		prometheusWatcher{unit: "bitcoind", expression: "bitcoind_ibd", server: prometheusURL, interval: 10 * time.Second, trigger: triggerPrometheusBitcoindIDB, events: events, errs: errs},
 	}
 }
 
@@ -282,20 +293,18 @@ func startWatchers(ws watchers) {
 }
 
 // parseEvent checks a string for relevant events and potentially returns an event type
-func parseEvent(p []byte, unit string) *serviceEvent {
+func parseEvent(p []byte, unit string) *watcherEvent {
 	switch {
 	case strings.Contains(string(p), "finished full compaction"): // fully synched electrs
-		return &serviceEvent{unit: unit, trigger: triggerElectrsFullySynced}
-
+		return &watcherEvent{unit: unit, trigger: triggerElectrsFullySynced}
 	case strings.Contains(string(p), "WARN - reconnecting to bitcoind: no reply from daemon"): // electrs unable to connect bitcoind
-		return &serviceEvent{unit: unit, trigger: triggerElectrsNoBitcoindConnectivity}
-
+		return &watcherEvent{unit: unit, trigger: triggerElectrsNoBitcoindConnectivity}
 	}
-
 	return nil
 }
 
-func processEvents(events chan serviceEvent, errs chan error) {
+// eventLoop loops indefinitely and processes incoming events
+func eventLoop(events chan watcherEvent, errs chan error, pState *supervisorState) {
 	for {
 		select {
 		case err := <-errs:
@@ -303,28 +312,45 @@ func processEvents(events chan serviceEvent, errs chan error) {
 		case event := <-events:
 			switch {
 			case event.trigger == triggerElectrsFullySynced:
-				handleElectrsFullySynced(event)
+				handleElectrsFullySynced(event, pState)
 			case event.trigger == triggerElectrsNoBitcoindConnectivity:
-				handleElectrsNoBitcoindConnectivity(event)
-			case event.trigger == triggerPrometheusMeasureUpdate:
-
-				fmt.Printf("Unit %v update: %v is now %v\n", event.unit, event.measure, event.value)
+				handleElectrsNoBitcoindConnectivity(event, pState)
+			case event.trigger == triggerPrometheusBitcoindIDB:
+				err := handleBitcoindIDB(event, pState)
+				if err != nil {
+					fmt.Printf("Could not handleBitcoindIDB: %s", err)
+				}
 			}
 		}
 	}
 }
 
+// checks if a trigger is flooding:
+// returns true if the trigger was executed under `minDelay` time ago
+func isTriggerFlooding(minDelay time.Duration, t trigger, pState *supervisorState) (isFlooding bool) {
+	if lastTimeTriggered, exists := pState.triggerLastExecuted[t]; exists {
+		timeSinceLastTrigger := time.Now().Sub(time.Unix(lastTimeTriggered, 0))
+		if timeSinceLastTrigger < minDelay {
+			fmt.Printf("Trigger %s is flodding. Last executed %v (minDelay %v)", t.String(), timeSinceLastTrigger, minDelay)
+			return true // last trigger less than `minDelay` ago
+		}
+		return false
+	} else {
+		fmt.Printf("No entry for that trigger exist. It can't be flooding.\n")
+		return false // no entry for that trigger exist. It can't be flooding.
+	}
+}
+
 // handleElectrsFullySynced restarts electrs after the initial sync is complete
-func handleElectrsFullySynced(event serviceEvent) {
-	switch event.unit {
-	case "electrs":
-		fmt.Printf("Electrs fully synced: %v\n", event.trigger)
+func handleElectrsFullySynced(event watcherEvent, pState *supervisorState) {
+	if isTriggerFlooding(30*time.Second, event.trigger, pState) == false {
+		fmt.Printf("Handling trigger %s: restarting Electrs.\n", event.trigger.String())
 		err := restartUnit("electrs")
 		if err != nil {
 			fmt.Errorf("Handling trigger %s: Restarting electrs failed: %v", event.trigger.String(), err)
+			return
 		}
-	default:
-		fmt.Printf("Message %v not defined for unit %v\n", event.trigger, event.unit)
+		pState.triggerLastExecuted[event.trigger] = time.Now().Unix()
 	}
 }
 
@@ -335,28 +361,93 @@ func restartUnit(unit string) error {
 	cmdAsString := "systemctl " + strings.Join(args, " ")
 	err := cmd.Run()
 	if err != nil {
-		return fmt.Errorf("Command  threw an error %v", cmdAsString, err)
+		return fmt.Errorf("Command %s threw an error %v", cmdAsString, err)
 	}
-	fmt.Printf("restartUnit: command '%v' executed.'", cmdAsString)
+	fmt.Printf("restartUnit: command '%v' executed.\n", cmdAsString)
 	return nil
 }
 
-func handleElectrsNoBitcoindConnectivity(event serviceEvent) {
-	fmt.Printf("Handling trigger %s: restarting Electrs to recreate the bitcoind `.cookie` file.\n", event.trigger.String())
-	err := restartUnit("electrs")
+func setBBBConfigValue(argument string, value string) error {
+	args := []string{"set", argument, value}
+	executable := "/usr/local/sbin/bbb-config.sh"
+	cmd := exec.Command(executable, args...)
+	cmdAsString := executable + " " + strings.Join(args, " ")
+	err := cmd.Run()
 	if err != nil {
-		fmt.Errorf("Handling trigger %s: Restarting electrs failed: %v", event.trigger.String(), err)
+		return fmt.Errorf("Command %s threw an error %v", cmdAsString, err)
 	}
+	fmt.Printf("setBBBConfigValue: command '%v' executed.\n", cmdAsString)
+	return nil
+}
+
+// handleElectrsNoBitcoindConnectivity handles the triggerElectrsNoBitcoindConnectivity
+// by restarting electrs which copies the current .cookie file
+func handleElectrsNoBitcoindConnectivity(event watcherEvent, pState *supervisorState) {
+	if isTriggerFlooding(30*time.Second, event.trigger, pState) == false {
+		fmt.Printf("Handling trigger %s: restarting electrs to recreate the bitcoind `.cookie` file.\n", event.trigger.String())
+		err := restartUnit("electrs")
+		if err != nil {
+			fmt.Errorf("Handling trigger %s: Restarting electrs failed: %v", event.trigger.String(), err)
+		}
+		pState.triggerLastExecuted[event.trigger] = time.Now().Unix()
+	}
+}
+
+// handleBitcoindIDB handles the triggerPrometheusBitcoindIDB
+// by setting (true) or unsetting (false) `bitcoin_idb` via bbb-config.sh
+func handleBitcoindIDB(event watcherEvent, pState *supervisorState) error {
+	oldValue, newValue := pState.prometheusLastStateIBD, event.value
+	// check if newValue is valid (either 1 or 0)
+	if newValue != 1 && newValue != 0 {
+		return fmt.Errorf("Handling trigger %s: newValue (%f) is invalid. Should be either 1 (IDB active) or 0 (IDB inactive)", event.trigger.String(), newValue)
+	}
+
+	if oldValue == newValue {
+		return nil // no state change (do nothing)
+	} else if oldValue == -1 {
+		// There is no prior state. Set `bitcoin_ibd` via bbbconfig.sh to true or false  (depending on the new state) just to be sure.
+		if newValue == 1 {
+			err := setBBBConfigValue("bitcoin_ibd", "true")
+			if err != nil {
+				return fmt.Errorf("Handling trigger %s: Initial set. Setting BBB config value to `true` failed: %v", event.trigger.String(), err)
+			}
+		} else {
+			err := setBBBConfigValue("bitcoin_ibd", "false")
+			if err != nil {
+				return fmt.Errorf("Handling trigger %s: Initial set. Setting BBB config value `false` failed: %v", event.trigger.String(), err)
+			}
+		}
+		pState.prometheusLastStateIBD = newValue // set the initial value for the state
+	} else if oldValue == 1 && newValue == 0 { // IDB finished
+		err := setBBBConfigValue("bitcoin_ibd", "false")
+		if err != nil {
+			return fmt.Errorf("Handling trigger %s: setting BBB config value to `false` failed: %v", event.trigger.String(), err)
+		}
+		pState.prometheusLastStateIBD = newValue
+	} else if oldValue == 0 && newValue == 1 { // IDB (re)started
+		err := setBBBConfigValue("bitcoin_ibd", "true")
+		if err != nil {
+			return fmt.Errorf("Handling trigger %s: setting BBB config value to `true` failed: %v", event.trigger.String(), err)
+		}
+		pState.prometheusLastStateIBD = newValue
+	}
+	return nil
 }
 
 func main() {
 	handleFlags()
 	printVersion()
 
-	events := make(chan serviceEvent) // channel to process events a watcher detects
+	events := make(chan watcherEvent) // channel to process events a watcher detects
 	errs := make(chan error)          // channel to process errors from watchers
+
+	// initialize the initial and empty state
+	state := supervisorState{
+		triggerLastExecuted:    make(map[trigger]int64),
+		prometheusLastStateIBD: -1,
+	}
 
 	ws := setupWatchers(events, errs)
 	startWatchers(ws)
-	processEvents(events, errs)
+	eventLoop(events, errs, &state) // this is passed as a pointer
 }
