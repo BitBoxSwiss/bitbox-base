dba0e9daf412b48211aebbf1fcfe591ed43072d5
TheCharlatan <seb.kung@gmail.com>
2019-08-09 18:26:10 +0200

    Middleware: Document RPCConn role as an io.ReadWriteCloser

--- middleware/src/rpcserver/rpcserver.go
+++ middleware/src/rpcserver/rpcserver.go
@@ -7,11 +7,13 @@ import (
 	"github.com/digitalbitbox/bitbox-base/middleware/src/rpcmessages"
 )
 
+// rpcConn wraps an io.ReadWriteCloser
 type rpcConn struct {
 	readChan  chan []byte
 	writeChan chan []byte
 }
 
+// newRPCConn returns an rpcConn struct that can be used as an interface to an io.ReadWriteCloser
 func newRPCConn() *rpcConn {
 	RPCConn := &rpcConn{
 		readChan:  make(chan []byte),
@@ -28,16 +30,19 @@ func (conn *rpcConn) WriteChan() chan []byte {
 	return conn.writeChan
 }
 
+// Read implements io.ReadWriteCloser
 func (conn *rpcConn) Read(p []byte) (n int, err error) {
 	message := <-conn.readChan
 	return copy(p, message), nil
 }
 
+// Write implements io.ReadWriteCloser
 func (conn *rpcConn) Write(p []byte) (n int, err error) {
 	conn.writeChan <- append([]byte(rpcmessages.OpRPCCall), p...)
 	return len(p), nil
 }
 
+// Close implements io.ReadWriteCloser. It is just a dummy function.
 func (conn *rpcConn) Close() error {
 	return nil
 }
@@ -59,7 +64,9 @@ type RPCServer struct {
 // NewRPCServer returns a new RPCServer
 func NewRPCServer(middleware Middleware) *RPCServer {
 	server := &RPCServer{
-		middleware:    middleware,
+		middleware: middleware,
+
+		//RPCConnection accepts an io.ReadWriteCloser interface from newRPCConn()
 		RPCConnection: newRPCConn(),
 	}
 	err := rpc.Register(server)
