4012fcf2279c3698dbff2412903210f373a129f2
Stadicus <stadicus@protonmail.com>
2019-08-21 21:59:53 +0200

    bbb-cmd: rename usb_flashdrive to flashdrive
    for consistency with bbbmiddleware

--- armbian/base/scripts/bbb-cmd.sh
+++ armbian/base/scripts/bbb-cmd.sh
@@ -16,7 +16,7 @@ possible commands:
   base restart      restarts the node
   base shutdown     shuts down the node
   
-  usb_flashdrive    <check|mount|umount>
+  flashdrive        <check|mount|umount>
   backup            <sysconfig|hsm_secret>
   restore           <sysconfig|hsm_secret>
                     
@@ -96,13 +96,13 @@ case "${MODULE}" in
         esac
         ;;
 
-    USB_FLASHDRIVE)
+    FLASHDRIVE)
         # sanitize device name
         ARG="${ARG//[^a-zA-Z0-9_\/]/}"
 
         case "${COMMAND}" in
             CHECK)
-                usb_flashdrive_count=0
+                flashdrive_count=0
                 while read -r scsidev; do
                     name=$( echo "${scsidev}" | cut -s -f 1 -d " " )
                     size=$( echo "${scsidev}" | cut -s -f 2 -d " " )
@@ -110,21 +110,21 @@ case "${MODULE}" in
 
                     # search drives that have a filesystem (not iso9660) and are <= 64GB
                     if [ -n "${fstype}" ] && [[ "${fstype}" != "iso9660" ]] && [[ size -lt 64000000000 ]]; then
-                        usb_flashdrive_count=$((usb_flashdrive_count + 1))
-                        usb_flashdrive_name="${name}"
+                        flashdrive_count=$((flashdrive_count + 1))
+                        flashdrive_name="${name}"
                     fi
                 done <<< "$(lsblk -o NAME,SIZE,FSTYPE -abrnp -I 8)"
 
                 # only 1 drive must be present, otherwise abort
-                if [[ usb_flashdrive_count -eq 1 ]]; then
-                    echo "${usb_flashdrive_name}"
+                if [[ flashdrive_count -eq 1 ]]; then
+                    echo "${flashdrive_name}"
 
-                elif [[ usb_flashdrive_count -eq 0 ]]; then
-                    echo "USB_FLASHDRIVE CHECK: no target found"
+                elif [[ flashdrive_count -eq 0 ]]; then
+                    echo "FLASHDRIVE CHECK: no target found"
                     exit 1
 
                 else
-                    echo "USB_FLASHDRIVE CHECK: too many targets found (${usb_flashdrive_count} in total)"
+                    echo "FLASHDRIVE CHECK: too many targets found (${flashdrive_count} in total)"
                     exit 1
                 fi               
                 ;;
@@ -133,13 +133,13 @@ case "${MODULE}" in
                 # ensure mountpoint is available
                 mkdir -p /mnt/backup
 
-                # check if ARG is valid USB flashdrive
+                # check if ARG is valid flashdrive
                 if ! lsblk "${ARG}" > /dev/null 2>&1; then
-                    echo "USB_FLASHDRIVE MOUNT: device ${ARG} not found."
+                    echo "FLASHDRIVE MOUNT: device ${ARG} not found."
                     exit 1
 
                 elif [ "$(lsblk -o NAME,SIZE,FSTYPE -abrnp -I 8 "${ARG}" | wc -l)" -ne 1 ]; then
-                    echo "USB_FLASHDRIVE MOUNT: device ${ARG} is not unique and/or has partitions."
+                    echo "FLASHDRIVE MOUNT: device ${ARG} is not unique and/or has partitions."
                     exit 1
 
                 else
@@ -150,7 +150,7 @@ case "${MODULE}" in
 
                     if [ -n "${fstype}" ] && [[ "${fstype}" != "iso9660" ]] && [[ size -lt 64000000000 ]]; then
 
-                        # mount USB device with the following options:
+                        # mount usb flashdrive with the following options:
                         #   rw:         read/write
                         #   nosuid:     cannot contain set userid files, prevents root escalation
                         #   nodev:      cannot contain special devices
@@ -159,10 +159,10 @@ case "${MODULE}" in
                         #   nodiratime: no update of directory access time
                         #   sync:       synchronous write, flushed to disk immediatly
                         mount "${ARG}" -o rw,nosuid,nodev,noexec,noatime,nodiratime,sync /mnt/backup
-                        echo "USB_FLASHDRIVE MOUNT: mounted ${name} to /mnt/backup"
+                        echo "FLASHDRIVE MOUNT: mounted ${name} to /mnt/backup"
 
                     else
-                        echo "USB_FLASHDRIVE MOUNT: device ${name} is either bigger than 64GB (${size}) or does the filesystem (${fstype}) is not supported."
+                        echo "FLASHDRIVE MOUNT: device ${name} is either bigger than 64GB (${size}) or does the filesystem (${fstype}) is not supported."
                         exit 1
                     fi
 
@@ -171,11 +171,11 @@ case "${MODULE}" in
 
             UNMOUNT)
                 if ! mountpoint /mnt/backup -q; then
-                    echo "USB_FLASHDRIVE UNMOUNT: no drive mounted at /mnt/backup"
+                    echo "FLASHDRIVE UNMOUNT: no drive mounted at /mnt/backup"
                     exit 1
                 else
                     umount /mnt/backup
-                    echo "USB_FLASHDRIVE UNMOUNT: /mnt/backup unmounted."
+                    echo "FLASHDRIVE UNMOUNT: /mnt/backup unmounted."
                 fi
                 ;;
 
