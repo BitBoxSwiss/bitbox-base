aecb4a277ec4f02304e8a477339f604ba8ccafd3
Stadicus <stadicus@protonmail.com>
2019-08-21 12:09:22 +0200

    bbb-cmd: improve usb_flashdrive / add backup (#152)
    * bbb-cmd: add 'backup' commands
    
    * bbb-cmd: improve usb_flashdrive
    
    * rename 'usb_thumbdrive' to 'usb_flashdrive'
    * recognize usb drives without partitions
    * improve error messages
    
    * bbb-cmd: imlement backup of Redis dbdump and hsm_secret
    
    * create / restore backup of Redis database file to /mnt/backup
    * create / restore backup of '/mnt/ssd/bitcoin/.lightning/hsm_secret' into Redis key lightningd:hsm_secret

--- armbian/base/scripts/bbb-cmd.sh
+++ armbian/base/scripts/bbb-cmd.sh
@@ -10,15 +10,16 @@ function usage() {
 usage: bbb-cmd.sh [--version] [--help] <command>
 
 possible commands:
-  bitcoind reindex   wipes UTXO set and validates existing blocks
-  bitcoind resync    re-download and validate all blocks
+  bitcoind reindex  wipes UTXO set and validates existing blocks
+  bitcoind resync   re-download and validate all blocks
 
   base restart      restarts the node
   base shutdown     shuts down the node
   
-  usb_thumbdrive    <check|mount|umount>
-  usb_backup
-
+  usb_flashdrive    <check|mount|umount>
+  backup            <sysconfig|hsm_secret>
+  restore           <sysconfig|hsm_secret>
+                    
 "
 }
 
@@ -94,60 +95,60 @@ case "${MODULE}" in
         esac
         ;;
 
-    USB_THUMBDRIVE)
+    USB_FLASHDRIVE)
         # sanitize device name
         ARG="${ARG//[^a-zA-Z0-9_\/]/}"
 
         case "${COMMAND}" in
             CHECK)
-                usb_thumbdrive_count=0
-                while read scsidev; do
-                    name=$( echo "${scsidev}" | cut -f 1 -d " " )
-                    size=$( echo "${scsidev}" | cut -f 2 -d " " )
-                    type=$( echo "${scsidev}" | cut -f 3 -d " " )
+                usb_flashdrive_count=0
+                while read -r scsidev; do
+                    name=$( echo "${scsidev}" | cut -s -f 1 -d " " )
+                    size=$( echo "${scsidev}" | cut -s -f 2 -d " " )
+                    fstype=$( echo "${scsidev}" | cut -s -f 3 -d " " )
 
-                    # search drives that have partition and are <= 64GB
-                    if [[ "${type}" == "part" ]] && [[ size -lt 64000000000 ]]; then
-                        usb_thumbdrive_count=$((usb_thumbdrive_count + 1))
-                        usb_thumbdrive_name="${name}"
+                    # search drives that have a filesystem (not iso9660) and are <= 64GB
+                    if [ -n "${fstype}" ] && [[ "${fstype}" != "iso9660" ]] && [[ size -lt 64000000000 ]]; then
+                        usb_flashdrive_count=$((usb_flashdrive_count + 1))
+                        usb_flashdrive_name="${name}"
                     fi
-                done <<< "$(lsblk -o NAME,SIZE,TYPE -abrnp -I 8)"
+                done <<< "$(lsblk -o NAME,SIZE,FSTYPE -abrnp -I 8)"
 
                 # only 1 drive must be present, otherwise abort
-                if [[ usb_thumbdrive_count -eq 1 ]]; then
-                    echo "${usb_thumbdrive_name}"
+                if [[ usb_flashdrive_count -eq 1 ]]; then
+                    echo "${usb_flashdrive_name}"
 
-                elif [[ usb_thumbdrive_count -eq 0 ]]; then
-                    echo "USB_THUMBDRIVE CHECK: no target found"
+                elif [[ usb_flashdrive_count -eq 0 ]]; then
+                    echo "USB_FLASHDRIVE CHECK: no target found"
                     exit 1
 
                 else
-                    echo "USB_THUMBDRIVE CHECK: too many targets found (${usb_thumbdrive_count} in total)"
+                    echo "USB_FLASHDRIVE CHECK: too many targets found (${usb_flashdrive_count} in total)"
                     exit 1
                 fi               
                 ;;
 
             MOUNT)
-                # check if ARG is valid USB thumbdrive
+                # ensure mountpoint is available
+                mkdir -p /mnt/backup
+
+                # check if ARG is valid USB flashdrive
                 if ! lsblk "${ARG}" > /dev/null 2>&1; then
-                    echo "USB_THUMBDRIVE MOUNT: device ${ARG} not found."
+                    echo "USB_FLASHDRIVE MOUNT: device ${ARG} not found."
                     exit 1
 
-                elif [ `lsblk -o NAME,SIZE,TYPE -abrnp -I 8 ${ARG} | wc -l` -ne 1 ]; then
-                    echo "USB_THUMBDRIVE MOUNT: device ${ARG} is not unique and/or has partitions."
+                elif [ "$(lsblk -o NAME,SIZE,FSTYPE -abrnp -I 8 "${ARG}" | wc -l)" -ne 1 ]; then
+                    echo "USB_FLASHDRIVE MOUNT: device ${ARG} is not unique and/or has partitions."
                     exit 1
 
                 else
-                    scsidev=`lsblk -o NAME,SIZE,TYPE -abrnp -I 8 ${ARG}`
-                    size=$( echo "${scsidev}" | cut -f 2 -d " " )
-                    type=$( echo "${scsidev}" | cut -f 3 -d " " )
+                    scsidev=$(lsblk -o NAME,SIZE,FSTYPE -abrnp -I 8 "${ARG}")
+                    name=$( echo "${scsidev}" | cut -s -f 1 -d " " )
+                    size=$( echo "${scsidev}" | cut -s -f 2 -d " " )
+                    fstype=$( echo "${scsidev}" | cut -s -f 3 -d " " )
 
-                    if [[ "${type}" != "part" ]] || [[ size -gt 64000000000 ]]; then
-                        echo "USB_THUMBDRIVE MOUNT: device ${scsidev} is either bigger than 64GB (${size}) or not a partition (type ${type})."
-                        exit 1
+                    if [ -n "${fstype}" ] && [[ "${fstype}" != "iso9660" ]] && [[ size -lt 64000000000 ]]; then
 
-                    # all checks passed
-                    else
                         # mount USB device with the following options:
                         #   rw:         read/write
                         #   nosuid:     cannot contain set userid files, prevents root escalation
@@ -157,7 +158,11 @@ case "${MODULE}" in
                         #   nodiratime: no update of directory access time
                         #   sync:       synchronous write, flushed to disk immediatly
                         mount "${ARG}" -o rw,nosuid,nodev,noexec,noatime,nodiratime,sync /mnt/backup
-                        echo "USB_THUMBDRIVE MOUNT: mounted ${scsidev}} to /mnt/backup"
+                        echo "USB_FLASHDRIVE MOUNT: mounted ${name} to /mnt/backup"
+
+                    else
+                        echo "USB_FLASHDRIVE MOUNT: device ${name} is either bigger than 64GB (${size}) or does the filesystem (${fstype}) is not supported."
+                        exit 1
                     fi
 
                 fi
@@ -165,17 +170,85 @@ case "${MODULE}" in
 
             UNMOUNT)
                 if ! mountpoint /mnt/backup -q; then
-                    echo "USB_THUMBDRIVE UNMOUNT: no drive mounted at /mnt/backup"
+                    echo "USB_FLASHDRIVE UNMOUNT: no drive mounted at /mnt/backup"
                 else
-                    echo "USB_THUMBDRIVE UNMOUNT: /mnt/backup unmounted."
                     umount /mnt/backup
+                    echo "USB_FLASHDRIVE UNMOUNT: /mnt/backup unmounted."
                 fi
                 ;;
+
             *)
                 echo "Invalid argument for module ${MODULE}: command ${COMMAND} unknown."
         esac
         ;;        
+    
+    BACKUP)
+        REDIS_FILEPATH="/data/redis/bitboxbase.rdb"
+        HSM_FILEPATH="/mnt/ssd/bitcoin/.lightning/hsm_secret"
+
+        case "${COMMAND}" in
+            # backup system configuration to mounted usb flashdrive
+            SYSCONFIG)
+                if mountpoint /mnt/backup -q; then
+                    cp "${REDIS_FILEPATH}" "/mnt/backup/bbb-backup.rdb"
+
+                    # create backup history (restore not yet implemented)
+                    cp "${REDIS_FILEPATH}" "/mnt/backup/bbb-backup_$(date '+%Y%m%d-%H%M').rdb"
+                else
+                    echo "ERR: /mnt/backup is not a mountpoint"
+                fi
+                echo "OK: backup created as /mnt/backup/bbb-backup.rdb"
+                ;;
+
+            # backup c-lightning on-chain keys in 'hsm_secret' into Redis database
+            HSM_SECRET)
+                # encode binary file 'hsm_secret' as base64 and store it in Redis
+                redis-cli SET lightningd:hsm_secret "$(base64 < ${HSM_FILEPATH})"
+                echo "OK: backup of file 'hsm_secret' created"
+                ;;
+
+            *)
+                echo "Invalid argument for module ${MODULE}: command ${COMMAND} unknown."
+        esac
+        ;;    
+
+    RESTORE)
+        REDIS_FILEPATH="/data/redis/bitboxbase.rdb"
+        HSM_FILEPATH="/mnt/ssd/bitcoin/.lightning/hsm_secret"
+
+        case "${COMMAND}" in
+            # restore system configuration from mounted usb flashdrive
+            SYSCONFIG)
+                if [ -f /mnt/backup/bbb-backup.rdb ]; then
+                    cp "/mnt/backup/bbb-backup.rdb" "${REDIS_FILEPATH}"
+                    systemctl restart redis-server.service
+                else
+                    echo "ERR: backup file /mnt/backup/bbb-backup.rdb not found"
+                    exit 1
+                fi
+                echo "OK: backup file /mnt/backup/bbb-backup.rdb restored to ${REDIS_FILEPATH}."
+                ;;
+
+            # restore c-lightning on-chain keys from Redis database
+            HSM_SECRET)
+                # create snapshot of 'hsm_secret'
+                if [ -f "${HSM_FILEPATH}" ]; then
+                    cp "${HSM_FILEPATH}" "${HSM_FILEPATH}_$(date '+%Y%m%d-%H%M').backup"
+                else
+                    echo "WARN: no previous 'hsm_secret' found, no local backup created"
+                fi
+
+                # save base64 encoded 'hsm_secret' as binary file to file system
+                # redis-cli causes script to terminate when Redis not available
+                redis-cli GET lightningd:hsm_secret | base64 -d > /mnt/ssd/bitcoin/.lightning/hsm_secret
+                echo "OK: backup of file 'hsm_secret' restored"
+                ;;
+
+            *)
+                echo "Invalid argument for module ${MODULE}: command ${COMMAND} unknown."
+        esac
+        ;;    
+
     *)
         echo "Invalid argument: module ${MODULE} unknown."
-
 esac
